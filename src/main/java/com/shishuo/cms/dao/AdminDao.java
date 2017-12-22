f7a7136f5a6561a697c7e3bd65c545b547b67106
zhouqinglei <527309557@qq.com>
2017-12-22 16:59:14 +0800

    1

--- src/main/java/com/shishuo/cms/dao/AdminDao.java
+++ src/main/java/com/shishuo/cms/dao/AdminDao.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  *	Copyright © 2013 Changsha Shishuo Network Technology Co., Ltd. All rights reserved.
  *	长沙市师说网络科技有限公司 版权所有
  *	http://www.shishuo.com
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Repository;
 
 import com.shishuo.cms.entity.Admin;
 import com.shishuo.cms.entity.vo.AdminVo;
+
 
 /**
  * 管理员
